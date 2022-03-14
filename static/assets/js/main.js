df97b6ffcce648f19d98e50b074b529e0c909ded
sumiyurup <50587837+sumiyurup@users.noreply.github.com>
2022-03-14 20:28:04 +0530

    new changes

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -155,3 +155,30 @@
     });
   });
 })();
+
+var icon = document.getElementById("icon");
+
+if (localStorage.getItem("theme") == "null") {
+  localStorage.setItem("theme", "dark");
+}
+
+let localData = localStorage.getItem("theme");
+
+if (localData == "dark") {
+  icon.src = "/assets/img/sun.png";
+  document.body.classList.remove("light-theme");
+} else if (localData == "light") {
+  icon.src = "/assets/img/moon.png";
+  document.body.classList.add("light-theme");
+}
+
+icon.onclick = function () {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
+    icon.src = "/assets/img/moon.png";
+    localStorage.setItem("theme", "light");
+  } else {
+    icon.src = "/assets/img/sun.png";
+    localStorage.setItem("theme", "dark");
+  }
+};
